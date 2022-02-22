09402f0749fa3fd3b35caa1d268555acb7019dba
“First <Kerger06@aol.com>
2022-02-21 20:53:16 -0600

    added health counter with working logic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+/** CLOCK LOGIC */
 const currentTime = () => {
     let fullDate = new Date();
     let hour = fullDate.getHours() % 12 || 12;
@@ -23,3 +23,49 @@ const currentTime = () => {
 } 
 
 setInterval(currentTime, 1000)
+
+/* HEALTH LOGIC*/ 
+//player 1
+    let healthP1 = 20;
+    const addP1 = document.getElementById('addHealthP1');
+    const subP1 = document.getElementById('subHealthP1');
+    const resetP1 = document.getElementById('resetHealthP1')
+    const healthHolderP1 = document.getElementById('healthCounterP1');
+    healthHolderP1.innerHTML = healthP1;
+    
+
+    addP1.addEventListener('click', () => {
+        healthHolderP1.innerHTML = ++healthP1 
+    });
+
+    subP1.addEventListener('click', () => {
+        healthHolderP1.innerHTML = --healthP1
+    });
+
+    resetP1.addEventListener('click', ()=> {
+        startingHealth = 20
+        healthHolderP1.innerHTML = healthP1
+    })
+
+//p2 health counter
+
+let healthP2 = 20;
+const addP2 = document.getElementById('addHealthP2');
+const subP2 = document.getElementById('subHealthP2');
+const resetP2 = document.getElementById('resetHealthP2');
+
+const healthHolderP2 = document.getElementById('healthCounterP2');
+healthHolderP2.innerHTML = healthP2
+
+addP2.addEventListener('click', ()=>{
+    healthHolderP2.innerHTML = ++healthP2
+})
+
+subP2.addEventListener('click', ()=>{
+    healthHolderP2.innerHTML = --healthP2
+})
+
+resetP2.addEventListener('click', ()=>{
+    healthP2 = 20
+    healthHolderP2.innerHTML = healthP2
+})
